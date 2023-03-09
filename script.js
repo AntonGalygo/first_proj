aeb315559fdf4f1b54ab83ce9214a41a9f006287
Anton <example@gmail.com>
2023-03-09 08:28:11 +0800

    movie db

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt("How much films did you watch?", '');
 
-const isChecked = true,
-    isClose = true;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
 
-console.log(isChecked === isClose);
+const a = prompt("Enter last watched film", '');
+const b = prompt("Rate this film", '');
+const c = prompt("Enter last watched film", '');
+const d = prompt("Rate this film", '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
